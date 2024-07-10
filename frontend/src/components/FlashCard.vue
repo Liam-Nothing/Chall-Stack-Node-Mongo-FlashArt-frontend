758a344740966b141a308f034d15eb2c6c08971e
Liam <liamf.pro+koesio@proton.me>
2024-07-10 10:36:54 +0200

    feat: Add FlashCard component

---
+++ frontend/src/components/FlashCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-card>
+      <v-img :src="flash.image" height="200px"></v-img>
+      <v-card-title>{{ flash.name }}</v-card-title>
+      <v-card-subtitle>{{ flash.description }}</v-card-subtitle>
+      <v-card-actions>
+        <v-btn text>Réserver</v-btn>
+      </v-card-actions>
+    </v-card>
+  </template>
+  
+  <script setup>
+  import { defineProps } from 'vue'
+  
+  const props = defineProps({
+    flash: {
+      type: Object,
+      required: true
+    }
+  })
+  </script>
+  
+  <style scoped>
+  </style>
+  
