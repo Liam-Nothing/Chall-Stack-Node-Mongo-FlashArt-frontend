1e96b6102a225ba4227271eb5e909e142d5f2b96
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2024-07-11 09:45:52 +0200

    feat: Add footer component

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-footer app color="primary" dark>
+      <v-spacer></v-spacer>
+      <span>&copy; 2024 Flash-Art-Chall</span>
+    </v-footer>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  </style>
+  
