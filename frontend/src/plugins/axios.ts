d886b3cfe9eea00192afba959a2c975b3a43c726
Liam <liamf.pro+koesio@proton.me>
2024-07-11 14:39:57 +0200

    feat: Add detection when token is expire

--- frontend/src/plugins/axios.ts
+++ frontend/src/plugins/axios.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useRouter } from 'vue-router';
 
 const apiClient = axios.create({
     baseURL: 'http://localhost:5000/api',
@@ -6,5 +7,35 @@ const apiClient = axios.create({
         'Content-type': 'application/json',
     },
 });
+
+// Add a request interceptor to add the token to headers
+apiClient.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+// Add a response interceptor to handle invalid token
+apiClient.interceptors.response.use(
+    response => {
+        return response;
+    },
+    error => {
+        if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+            localStorage.removeItem('token');
+            localStorage.removeItem('role');
+            const router = useRouter();
+            router.push('/login');
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default apiClient;
